0dbefbe4629af72e1bce5a6447ca06949bcd4e2b
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-05-18 22:45:36 -0300

    Aula 01.

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Olá JSON' });
+});
+
+app.post('/', (req, res) => {
+  return res.json({ message: 'Os dados foram salvos com sucesso!' });
+});
+
+app.listen(3333, () => {
+  console.log('Rodando na 3333...');
+});
