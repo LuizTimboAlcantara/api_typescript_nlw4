033e0afb52ab8f1a758424cc6c3d42ef530e2a33
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-05-19 22:07:32 -0300

    Aula 02 - parte 02

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Olá JSON' });
-});
-
-app.post('/', (req, res) => {
-  return res.json({ message: 'Os dados foram salvos com sucesso!' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
   console.log('Rodando na 3333...');
